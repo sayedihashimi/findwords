f3721cb6c9df416172d1ce5410960898b3374e67
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2019-11-29 01:35:47 -0500

    adding six letter word file

--- src/FindWords.Test/TestWordTreeBuilder.cs
+++ src/FindWords.Test/TestWordTreeBuilder.cs
@@ -39,11 +39,36 @@ accommodable";
                 Assert.True(tree.IsWord("mac"));
             }
 
+            /// <summary>
+            /// This can be used to re-create the six letter word file.
+            /// Normally this should be commented out.
+            /// </summary>
+            //[Fact]
+            //public async Task CreateSixLetterWordFileAsync() {
+            //    string destpath = Path.Combine(
+            //        GetApplicationRoot(),
+            //        $"test-sixletterwords-{DateTime.Now.ToString("yyyyMMdd-hh-mm-ss")}.txt");
+            //    using var readerFileStream = new FileStream(GetPathToRealDictionaryFile(), FileMode.Open,FileAccess.Read);
+            //    using var reader = new StreamReader(readerFileStream);
+
+            //    using var outputFileStream = new FileStream(destpath, FileMode.Create, FileAccess.ReadWrite);
+            //    using var outputWriter = new StreamWriter(outputFileStream);
+
+            //    string currentLine;
+            //    while( (currentLine = await reader.ReadLineAsync()) != null) {
+            //        if(currentLine.Length == 6) {
+            //            await outputWriter.WriteLineAsync(currentLine);
+            //        }
+            //    }
+
+            //    await outputWriter.FlushAsync();
+            //}
+
             internal string GetPathToRealDictionaryFile() {
                 return Path.Combine(GetApplicationRoot(), "assets/words_alpha.txt");
             }
 
-            public string GetApplicationRoot() {
+            internal string GetApplicationRoot() {
                 var exePath = Path.GetDirectoryName(System.Reflection
                                   .Assembly.GetExecutingAssembly().CodeBase);
                 Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
