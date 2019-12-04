3ac91872a04b5a0c21af0c477e189dbc643a99b8
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2019-12-03 23:40:02 -0500

    minor code cleanup

--- src/FindWords.Test/TestWordTreeBuilder.cs
+++ src/FindWords.Test/TestWordTreeBuilder.cs
@@ -38,68 +38,6 @@ accommodable";
                 Assert.True(tree.IsWord("ritualistically"));
                 Assert.True(tree.IsWord("mac"));
             }
-
-            /// <summary>
-            /// This can be used to re-create the six letter word file.
-            /// Normally this should be commented out.
-            /// </summary>
-            //[Fact]
-            //public async Task CreateSixLetterWordFileAsync() {
-            //    string destpath = Path.Combine(
-            //        GetApplicationRoot(),
-            //        $"test-sixletterwords-{DateTime.Now.ToString("yyyyMMdd-hh-mm-ss")}.txt");
-            //    using var readerFileStream = new FileStream(GetPathToRealDictionaryFile(), FileMode.Open,FileAccess.Read);
-            //    using var reader = new StreamReader(readerFileStream);
-
-            //    using var outputFileStream = new FileStream(destpath, FileMode.Create, FileAccess.ReadWrite);
-            //    using var outputWriter = new StreamWriter(outputFileStream);
-
-            //    string currentLine;
-            //    while( (currentLine = await reader.ReadLineAsync()) != null) {
-            //        if(currentLine.Length == 6) {
-            //            await outputWriter.WriteLineAsync(currentLine);
-            //        }
-            //    }
-
-            //    await outputWriter.FlushAsync();
-            //}
-
-            // can be used to remove words with special characters from
-            // the cspell file
-//            [Fact]
-//            public async Task PreprocessCspellFileAsync() {
-//                var hasNonAlphaCharsPattern = @"[^a-zA-Z]";
-//                var fileHelper = new FileHelper();
-//                var pathToCspellSourceFile = Path.Combine(
-//                    fileHelper.GetApplicationRoot(),
-//                    "assets","cspell-en_US.txt");
-
-//                var filteredWordsOutFilePath = Path.GetTempFileName();
-//                var removedWordsOutFilePath = Path.GetTempFileName();
-
-//                using var sourceFileStream = new FileStream(pathToCspellSourceFile, FileMode.Open, FileAccess.Read);
-//                using var sourceFileReader = new StreamReader(sourceFileStream);
-//                using var filteredOutStream = new FileStream(filteredWordsOutFilePath, FileMode.Create, FileAccess.ReadWrite);
-//                using var filteredOutWriter = new StreamWriter(filteredOutStream);
-//                using var removedOutStream = new FileStream(removedWordsOutFilePath, FileMode.Create, FileAccess.ReadWrite);
-//                using var removedOutWriter = new StreamWriter(removedOutStream);
-
-//                var nonAlphaRegex = new Regex(hasNonAlphaCharsPattern, RegexOptions.Compiled);
-//                string cspellLine;
-//                while( (cspellLine = await sourceFileReader.ReadLineAsync()) != null) {
-//                    if (nonAlphaRegex.IsMatch(cspellLine)) {
-//                        await removedOutWriter.WriteLineAsync(cspellLine);
-//                    }
-//                    else {
-//                        await filteredOutWriter.WriteLineAsync(cspellLine.ToLowerInvariant());
-//                    }
-//                }
-
-//                Console.WriteLine($@"
-//filtered words file: {filteredWordsOutFilePath}
-//removed words file: {removedWordsOutFilePath}
-//");
-//            }
         }
     }
 }
